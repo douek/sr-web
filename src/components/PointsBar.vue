a8188a4e39c7a492816e29cf49232596a8adf642
Daniel Douek <dadouek@gmail.com>
2020-04-07 18:38:39 +0300

    add radio selection and submit supermemo calc

--- src/components/PointsBar.vue
+++ src/components/PointsBar.vue
@@ -1,51 +1,85 @@
 <template>
   <div class="extra content">
-    <div class="ui form">
+    <div v-if="!submmited" class="ui form">
       <div class="inline fields">
-        <label for="points">Select your point reuslt:</label>
+        <label :for="getName">Select your points reuslt:</label>
         <div class="field">
           <div class="ui radio checkbox">
-            <input type="radio" name="points" checked tabindex="0" class="hidden" />
+            <input type="radio" :name="getName" v-model="quality" :value="0" checked="checked" />
             <label>0</label>
           </div>
         </div>
         <div class="field">
           <div class="ui radio checkbox">
-            <input type="radio" name="points" tabindex="0" class="hidden" />
+            <input type="radio" :name="getName" v-model="quality" :value="1" />
             <label>1</label>
           </div>
         </div>
         <div class="field">
           <div class="ui radio checkbox">
-            <input type="radio" name="points" tabindex="0" class="hidden" />
+            <input type="radio" :name="getName" v-model="quality" :value="2" />
             <label>2</label>
           </div>
         </div>
         <div class="field">
           <div class="ui radio checkbox">
-            <input type="radio" name="points" tabindex="0" class="hidden" />
+            <input type="radio" :name="getName" v-model="quality" :value="3" />
             <label>3</label>
           </div>
         </div>
         <div class="field">
           <div class="ui radio checkbox">
-            <input type="radio" name="points" tabindex="0" class="hidden" />
+            <input type="radio" :name="getName" v-model="quality" :value="4" />
             <label>4</label>
           </div>
         </div>
         <div class="field">
           <div class="ui radio checkbox">
-            <input type="radio" name="points" tabindex="0" class="hidden" />
+            <input type="radio" :name="getName" v-model="quality" :value="5" />
             <label>5</label>
           </div>
         </div>
       </div>
+      <div>
+        <div class="ui clearing divider"></div>
+        <button @click="onSubmit" class="ui green button">submit</button>
+      </div>
+    </div>
+    <div v-else class="ui success message">
+      <div>Points Submitted</div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
-  name: "PointsBar"
+  name: "PointsBar",
+  props: {
+    card: Object
+  },
+  data() {
+    return {
+      quality: "",
+      submmited:false
+    };
+  },
+  computed: {
+    getName() {
+      return ("card" + this.card.id);
+    }
+  },
+  methods: {
+    ...mapActions(['superMemoCardCalc']),
+    onSubmit(){
+      this.submmited = true;     
+      let input = {
+        card: this.card,
+        quality: this.quality
+      };
+
+      this.superMemoCardCalc(input)
+    }
+  }
 };
 </script>
--- src/components/Session.vue
+++ src/components/Session.vue
@@ -1,37 +1,57 @@
 <template>
-    <div class="ui continer">
-        <div class="ui raised very padded text container segment" v-for="c in allCards" :key="c.id">
-        <CardSession :card="c"/>
-        <div class="ui clearing divider"></div>
-        <PointsBar/>
-        </div>
-        <div>
+  <div class="ui continer">
+    <div v-if="clickedToStart">
+    <div
+      class="ui raised very padded text container segment"
+      v-for="c in getCardsForToday"
+      :key="c.id"
+    >
+      <CardSession :card="c" />
+      <div class="ui clearing divider"></div>
+      <PointsBar :card="c"/>
+    </div>
+    </div>
+        <button
+      class="ui primary button"
+      v-else
+      @click="startSession"
+    >Start today session</button>
+    <div class="ui clearing divider"></div>
+
+    <div>
       <p>
         5 - perfect response
         <br />4 - correct response after a hesitation
-        <br />3 - correct response recalled with serious difficulty
+        <br />3 - correct response recalled with serious difficulty or with hint
         <br />2 - incorrect response; where the correct one seemed easy to recall
         <br />1 - incorrect response; the correct one remembered
         <br />0 - complete blackout.
       </p>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import PointsBar from './PointsBar';
-import CardSession from './CardSession';
+import { mapGetters } from "vuex";
+import PointsBar from "./PointsBar";
+import CardSession from "./CardSession";
 
 export default {
-    name: 'Session',
-    components:{
-        CardSession,
-        PointsBar
+  name: "Session",
+  data() {
+    return { clickedToStart: false, };
+  },
+  components: {
+    CardSession,
+    PointsBar
+  },
+  computed: {
+    ...mapGetters(["getCardsForToday"])
+  },
+  methods: {
+    startSession(){
+      this.clickedToStart = true;
     },
-    computed:{
-        ...mapGetters(['allCards']),
-    }
-
-}
+  }
+};
 </script>
